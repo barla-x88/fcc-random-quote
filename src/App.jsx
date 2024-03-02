5a4614d854a43e7c77138a515f3f4b98b8994c6a
Barla Ankit <barla.x88@gmail.com>
2024-03-02 18:02:46 +0530

    Add loading indicator and tweet functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { PropagateLoader } from 'react-spinners';
 
 function App() {
-  const [quote, setQuote] = useState({});
+  const [quote, setQuote] = useState(null);
 
   const fetchNewQuote = async () => {
     const request = await fetch('https://api.quotable.io/random');
@@ -22,12 +23,20 @@ function App() {
     >
       <div className="container">
         <div className="row">
-          <q id="text" className="display-5">
-            {quote.content || 'Fetching Quote...'}
-          </q>
-          <cite id="author" className="d-block text-end mt-4 fs-4">
-            {quote.author}
-          </cite>
+          {quote ? (
+            <div>
+              <q id="text" className="display-5">
+                {quote.content}
+              </q>
+              <cite id="author" className="d-block text-end mt-4 fs-4">
+                {quote.author}
+              </cite>
+            </div>
+          ) : (
+            <div className="d-flex justify-content-center align-items-center">
+              <PropagateLoader color="#36d7b7" />
+            </div>
+          )}
         </div>
         <div className="row mt-5">
           <button
@@ -42,8 +51,11 @@ function App() {
         <div className="row mt-2">
           <a
             className="btn btn-primary p-2"
-            href="https://twitter.com/intent/tweet"
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+              '"' + quote?.content + '"' + '\n' + quote?.author || ''
+            )}`}
             id="tweet-quote"
+            target="_blank"
           >
             Tweet This quote
           </a>
